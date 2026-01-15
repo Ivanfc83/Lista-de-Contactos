b8c93b46934bb2cc126d5921ccb0eef7158a40d2
Iván <ivanfc2704@gmail.com>
2026-01-15 19:34:56 -0300

    CRUD con contactos en MockAPI y algunas correcciones de estilos

--- script.js
+++ script.js
@@ -2,71 +2,64 @@
 let contactos = [];
 let contactoEditando = null;
 
+const API_URL = "https://694332dd69b12460f313f684.mockapi.io/Contactos";
+
 const botonAgregar = document.getElementById('btnAgregar');
 
 botonAgregar.addEventListener('click', function(){
-    const datos = obtenerDatosFormulario();
+    
+   const datos = obtenerDatosFormulario();
+   console.log("DATOS FORM:", datos);
 
     if(!validarDatos(datos)){
-        alert('Completa todos los campos');
+        showSwalToast("Datos inválidos", "Revisá los campos (email incluido)", "error");;
         return;
     }
 
     if(contactoEditando){
-        contactoEditando.nombre = datos.nombre;
-        contactoEditando.email = datos.email;
-        contactoEditando.telefono = datos.telefono;
-        contactoEditando.fechaNacimiento = datos.fechaNacimiento;
-
-        
-        contactoEditando = null;
-        botonAgregar.textContent = 'Agregar';
-        botonAgregar.classList.remove('btn-success');
-        botonAgregar.classList.add('btn-primary')
-        showSwalToast("Contacto Editado!", "El contacto se editó correctamente", "info");
-
+        editarContactosAPI(contactoEditando.id, datos);
          
     }else{
-        const contacto = crearContacto(datos);
-        guardarContacto(contacto);
-
-        showSwalToast("Contacto Agregado!", "El contacto se agregó correctamente");
         
+        agregarContactoAPI(datos);
     }
     
    
 
-    mostrarContactos();
 });
 
 function obtenerDatosFormulario (){
     const nombre = document.getElementById('nombre').value;
     const email = document.getElementById('email').value;
     const telefono = document.getElementById('telefono').value;
-    const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    const fechaDeNacimiento = document.getElementById('fechaDeNacimiento').value;
 
     return {
         nombre,
         email,
         telefono,
-        fechaNacimiento
+        fechaDeNacimiento
     };
 }
 
 function validarDatos(datos){
-    if(datos.nombre === "" || datos.email === "" || datos.telefono === "" || datos.fechaNacimiento === ""){
+    if(datos.nombre === "" || datos.email === "" || datos.telefono === "" || datos.fechaDeNacimiento === ""){
         return false;
         }
+    
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(datos.email);
+    if(!emailValido) return false;
+
     return true;
 }
 
 function crearContacto (datos){
     return {
-        id: Date.now(),
+        // id: Date.now(),
         nombre: datos.nombre,
         email: datos.email,
         telefono: datos.telefono,
-        fechaNacimiento: datos.fechaNacimiento,
+        fechaDeNacimiento: datos.fechaDeNacimiento,
     };
 }
 
@@ -80,17 +73,20 @@ function mostrarContactos (){
     const tbody = document.getElementById('tablaContactos');
     tbody.innerHTML = "";
 
+    if (!Array.isArray(contactos)) return;
+    
     contactos.forEach( function (contacto) {
+        console.log(contacto.fechaDeNacimiento);
         const fila = `
         <tr>
-        <td>${contacto.nombre}</td>
-        <td>${contacto.email}</td>
-        <td>${contacto.telefono}</td>
-        <td>${contacto.fechaNacimiento}</td>
+        <td>${ contacto.nombre }</td>
+        <td><a href="mailto:${contacto.email}">${ contacto.email }</td>
+        <td>${ contacto.telefono }</td>
+        <td>${ formatearFecha(contacto.fechaDeNacimiento) }</td>
         <td>
-        <button class="btn btn-sm btn-primary" onclick = "editarContacto(${contacto.id})"><i class="fa-solid fa-pen"></i></button>
+        <button class="btn btn-sm btn-primary" onclick = "editarContacto('${ contacto.id }')"><i class="fa-solid fa-pen"></i></button>
        
-        <button class="btn btn-sm btn-danger" onclick = "eliminarContacto(${contacto.id})"><i class="fa-solid fa-trash"></i></button>
+        <button class="btn btn-sm btn-danger" onclick = "eliminarContacto('${ contacto.id }')"><i class="fa-solid fa-trash"></i></button>
         </td>
         `;
 
@@ -102,20 +98,15 @@ function limpiarFormulario(){
     document.getElementById('nombre').value = '';
     document.getElementById('email').value = '';
     document.getElementById('telefono').value = '';
-    document.getElementById('fechaNacimiento').value = '';
+    document.getElementById('fechaDeNacimiento').value = '';
 }
 
 function eliminarContacto (id){
     
-    const confirmar = confirm("¿Estás seguro de que deseas eliminar esta tarea?");
+    const confirmar = confirm("¿Estás seguro de que deseas eliminar éste contacto?");
 
     if(confirmar){
-        contactos = contactos.filter( function(contacto){
-            return contacto.id !== id;
-        });
-
-        showSwalToast("Contacto Eliminado", "El contacto se eliminó correctamente", "error");
-        mostrarContactos();
+        eliminarContactoAPI(id);
     }
 
 }
@@ -140,7 +131,7 @@ function editarContacto(id){
         document.getElementById('nombre').value = contactoEditando.nombre;
         document.getElementById('email').value = contactoEditando.email;
         document.getElementById('telefono').value = contactoEditando.telefono;
-        document.getElementById('fechaNacimiento').value = contactoEditando.fechaNacimiento;
+        document.getElementById('fechaDeNacimiento').value = contactoEditando.fechaDeNacimiento;
    
         botonAgregar.classList.remove("btn-primary");
         botonAgregar.classList.add("btn-success");
@@ -160,5 +151,96 @@ function showSwalToast(titulo, descripcion, iconito = "success") {
     toast: true,
     theme: "dark",
   });
+}
+
+
+async function cargarContactos() {
+  try {
+    const respuesta = await axios.get(API_URL);
+
+    contactos = respuesta.data;
+
+    console.log("CONTACTOS TRAIDOS:", contactos);
+    mostrarContactos();
+
+  } catch (error) {
+    showSwalToast('Error', 'No se pudieron cargar los contactos', 'error');
+  }
+}
+
+window.addEventListener('DOMContentLoaded', cargarContactos);
+
+async function agregarContactoAPI(datos){
+  try {
+    const respuesta = await axios.post(API_URL, datos);
+    console.log("RESPUESTA POST:", respuesta.data);
+
+    showSwalToast("Contacto Agregado!", "El contacto se agregó correctamente");
+
+    limpiarFormulario();
+    await cargarContactos();
+  } catch (error) {
+    showSwalToast('Error', 'No se pudieron cargar los contactos', 'error');
+  }
+
+  
+}
+
+async function editarContactosAPI(id, datos) {
+ try {
+  await axios.put(`${API_URL}/${id}`, datos);
+
+  showSwalToast('Contacto editado', 'Se actualizó correctamente', 'info');
+  
+  limpiarFormulario();
+
+  contactoEditando = null;
+  botonAgregar.textContent = 'Agregar';
+  botonAgregar.classList.remove('btn-success');
+  botonAgregar.classList.add('btn-primary');
+
+  await cargarContactos();
+
+ } catch (error) {
+  showSwalToast("Error", "No se pudo editar el contacto", "error");
+ }  
+}
+
+async function eliminarContactoAPI(id) {
+  try {
+    await axios.delete(`${API_URL}/${id}`);
+
+    showSwalToast('Contacto Eliminado', 'Se eliminó correctamente', 'error');
+    await cargarContactos();
+
+
+  } catch (error) {
+    showSwalToast("Error", "No se pudo eliminar el contacto", "error");
+  }
+  
+}
+
+function formatearFecha(fecha) {    
+  if(!fecha) return "";
+
+  // Si la fecha viene como "yyyy-mm-dd"
+  if(typeof fecha === 'string' && fecha.includes('-')){
+    const [yyyy, mm, dd] = fecha.split('-');
+    return `${dd}-${mm}-${yyyy}`;
+  }
+
+  //Si viene la fecha como número (timestamp)
+  const fechaNumero = Number(fecha);
+
+  //Si es timestamp en segundos (10 digitos aproximadamente)
+  const esSegundos = fechaNumero < 1000000000000;
+
+  const date = new Date(esSegundos ? fechaNumero * 1000 : fechaNumero);
+
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2,'0');
+  const dd = String(date.getDate()).padStart(2, '0');
+
+  return `${dd}-${mm}-${yyyy}`;
 }
 
